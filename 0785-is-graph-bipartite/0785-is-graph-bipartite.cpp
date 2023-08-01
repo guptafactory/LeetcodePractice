d659cf175ab2c31bd89ea4b78062dd386070a036
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-08-02 00:00:36 +0530

    Time: 19 ms (96.27%), Space: 13.3 MB (89.60%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    bool componentBipartite(int start, vector<int> &color, vector<vector<int>>& graph) {
-        queue<int> que;
-        que.push(start);
-        color[start] = 0;
-        while(!que.empty()) {
-            int node = que.front();
-            que.pop();
-            for(int adjNode: graph[node]) {
+    bool componentBipartite(int node, int presentColor, vector<int> &color, vector<vector<int>>& graph) {
+        color[node] = presentColor;
+        for(int adjNode: graph[node]) {
                 if(color[adjNode] == -1) {
-                    color[adjNode] = !color[node];
-                    que.push(adjNode);
+                    if(componentBipartite(adjNode, !color[node], color, graph) == false) return false;
                 }
-                else if(color[adjNode] == color[node]) return false;   
-            }
+                else if(color[adjNode] == color[node]) return false;
         }
         return true;
     }
@@ -22,7 +15,7 @@ public:
         vector<int> color(n, -1);
         for(int i = 0; i < n; i++) {
             if(color[i] == -1) {
-              if(componentBipartite(i, color, graph) == false) return false;
+              if(componentBipartite(i, 0, color, graph) == false) return false;
             } 
         }
         return true;
