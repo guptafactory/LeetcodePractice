e656d05ad5f79248494ac10e7468d1d31336cf18
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-08-03 03:41:57 +0530

    Time: 15 ms (97.92%), Space: 13.7 MB (67.69%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        vector<int> indegree(numCourses, 0), ans;
+        queue<int> que;
+        for(auto pr: prerequisites) {
+            adj[pr[1]].push_back(pr[0]);
+            indegree[pr[0]]++;
+        }
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                que.push(i);
+                ans.push_back(i);
+            }
+        }
+        while(!que.empty()) {
+            int node = que.front();
+            que.pop();
+            for(int adjNode: adj[node]) {
+                indegree[adjNode]--;
+                if(indegree[adjNode] == 0) {
+                    que.push(adjNode);
+                    ans.push_back(adjNode);
+                }
+            }
+        }
+        return ans.size() == numCourses ? true : false;
+    }
+};
