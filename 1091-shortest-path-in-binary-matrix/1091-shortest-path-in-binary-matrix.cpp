bcd38d2d57c6975f71d199f1ece339cb59f56b33
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-08-08 00:44:49 +0530

    Time: 55 ms (92.40%), Space: 22.4 MB (28.95%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -7,15 +7,15 @@ public:
         int delRow[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
         int delCol[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
         queue<pair<int, pair<int, int>>> que;
-        if(grid[0][0] == 0) { que.push({1, {0, 0}}); grid[0][0] = -1; }
+        if(grid[0][0] == 0) { que.push({1, {0, 0}}); grid[0][0] = 1; }
         while(!que.empty()) {
             dst = que.front().first, row = que.front().second.first, col = que.front().second.second;
             que.pop();
             if(row == n-1 && col == n-1) return dst;
             for(int k = 0; k < 8; k++) {
                 i = row + delRow[k], j = col + delCol[k];
-                if(i >=0 && i < n && j >= 0 && j < n && grid[i][j] == 0) {
-                    grid[i][j] = -1;
+                if(i >=0 && i < n && j >= 0 && j < n && grid[i][j] == 0 && dist[i][j] == INT_MAX) {
+                    dist[i][j] = dst + 1;
                     que.push({dst + 1, {i, j}});
                 }
             }
