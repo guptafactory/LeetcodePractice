acd29302892096f2d3d9ad6bfa7346727e2c6382
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-08-12 02:34:07 +0530

    Time: 91 ms (94.08%), Space: 42.8 MB (46.14%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,21 +1,58 @@
-class Solution {
+class DisjointSet {
+    vector<int> rank, parent, size;
 public:
-    void dfs(int node, vector<int> &vis, vector<int> adj[]) {
-        vis[node] = 1;
-        for(int adjNode: adj[node]) {
-            if(!vis[adjNode]) dfs(adjNode, vis, adj);
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        vector<int> adj[n];
-        vector<int> vis(n, 0);
-        int edges = connections.size(), component = 0;
-        for(vector<int> it: connections) {
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+    int findUPr(int node) {
+        if (node == parent[node])
+            return node;
+        else
+            return parent[node] = findUPr(parent[node]);
+    }
+    void unionByRank(int u, int v) {
+        int upar_u = findUPr(u);
+        int upar_v = findUPr(v);
+        if (upar_u == upar_v)
+            return;
+        if (rank[upar_u] < rank[upar_v])
+            parent[upar_u] = upar_v;
+        else if (rank[upar_u] > rank[upar_v])
+            parent[upar_v] = upar_u;
+        else {
+            parent[upar_v] = upar_u;
+            rank[upar_u]++;
         }
+    }
+    void unionBySize(int u, int v) {
+        int upar_u = findUPr(u);
+        int upar_v = findUPr(v);
+        if (upar_u == upar_v)
+            return;
+        if (size[upar_u] < size[upar_v]) {
+            parent[upar_u] = upar_v;
+            size[upar_v] += size[upar_u];
+        }
+        else {
+            parent[upar_v] = upar_u;
+            size[upar_u] += size[upar_v];
+        }
+    }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int edges = connections.size(), component = 0;
+        DisjointSet ds(n);
+        for(vector<int> it: connections) ds.unionByRank(it[0], it[1]);
         for(int i = 0; i < n; i++) {
-            if(!vis[i]) { dfs(i, vis, adj); component++; }
+            if (ds.findUPr(i) == i) component++;
         }
         return (edges < n - 1) ? -1 : component-1;
     }
