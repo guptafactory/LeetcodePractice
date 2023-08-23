0ab27a8dcb0f2386a98993942a6cbd77896f3273
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-08-24 00:43:44 +0530

    Time: 3 ms (53.74%), Space: 7.9 MB (36.27%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,25 +2,21 @@ class Solution {
 private:
     vector<vector<int>> ans;
     map<int, bool> mp;
-    void permutation(vector<int> &arr, int &n, vector<int>& nums) {
-        if(arr.size() == n) {
-            ans.push_back(arr);
+    void permutation(int ind, int &n, vector<int>& nums) {
+        if(ind == n) {
+            ans.push_back(nums);
             return;
         }
-        for(int i = 0; i < n; i++) {
-            if(mp[i] == 1) continue;
-            arr.push_back(nums[i]);
-            mp[i] = 1;
-            permutation(arr, n, nums);
-            arr.pop_back();
-            mp[i] = 0;
+        for(int i = ind; i < n; i++) {
+            swap(nums[i], nums[ind]);
+            permutation(ind + 1, n, nums);
+            swap(nums[i], nums[ind]);
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
-        vector<int> arr;
-        permutation(arr, n, nums);
+        permutation(0, n, nums);
         return ans;
     }
 };
