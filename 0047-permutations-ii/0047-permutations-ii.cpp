baaca8df6670ea1ff0ba5d2b95f2f06260cdd40d
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-08-24 00:47:54 +0530

    Time: 41 ms (36.91%), Space: 9.7 MB (39.88%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,26 +1,21 @@
 class Solution {
 private:
     set<vector<int>> st;
-    map<int, bool> mp;
-    void permutation(vector<int> &arr, int &n, vector<int>& nums) {
-        if(arr.size() == n) {
-            st.insert(arr);
+    void permutation(int ind, int &n, vector<int>& nums) {
+        if(ind == n) {
+            st.insert(nums);
             return;
         }
-        for(int i = 0; i < n; i++) {
-            if(mp[i] == 1) continue;
-            arr.push_back(nums[i]);
-            mp[i] = 1;
-            permutation(arr, n, nums);
-            arr.pop_back();
-            mp[i] = 0;
+        for(int i = ind; i < n; i++) {
+            swap(nums[i], nums[ind]);
+            permutation(ind + 1, n, nums);
+            swap(nums[i], nums[ind]);
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         int n = nums.size();
-        vector<int> arr;
-        permutation(arr, n, nums);
+        permutation(0, n, nums);
         vector<vector<int>> ans;
         for(auto x: st) ans.push_back(x);
         return ans;
