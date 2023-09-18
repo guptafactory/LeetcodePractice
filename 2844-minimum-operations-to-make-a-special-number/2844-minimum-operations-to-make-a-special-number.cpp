a41d8211f6a49962af7d3b9cdf253c7ca4f9be33
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-09-19 02:23:12 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (15.07%) - LeetHub

---
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumOperations(string num) {
+        int n = num.size();
+        bool five = false, zero = false;
+        for(int i = n-1; i >= 0; i--) {
+            if(zero && (num[i] == '0' || num[i] == '5')) return n-i-2; // 00, 50 
+            if(five && (num[i] == '2' || num[i] == '7')) return n-i-2; // 25, 75
+            if(num[i] == '0') zero = true;
+            if(num[i] == '5') five = true;
+        }
+        if(zero) return n-1; // one zero is present, rest remove all digits
+        return n; // no zero is present, delete all digits, 0 is divisible by 25
+    }
+};
