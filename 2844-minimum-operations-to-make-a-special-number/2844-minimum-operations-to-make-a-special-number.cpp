a5e727d8916271bda5c61ed1213de0d116923ec7
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-09-19 02:27:36 +0530

    Time: 2 ms (59.12%), Space: 6.6 MB (12.18%) - LeetHub

--- 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
@@ -6,6 +6,8 @@ public:
         for(int i = n-1; i >= 0; i--) {
             if(zero && (num[i] == '0' || num[i] == '5')) return n-i-2; // 00, 50 
             if(five && (num[i] == '2' || num[i] == '7')) return n-i-2; // 25, 75
+            // n-i -> represents sliding window like [5, 7, 7, 0]
+            // subtract 2 values from this, as these 2 digits are to be saved (no deletion)
             if(num[i] == '0') zero = true;
             if(num[i] == '5') five = true;
         }
