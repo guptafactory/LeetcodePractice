43d55e79531239a5e0de64c66da2557bc756a7fd
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-09-19 20:20:06 +0530

    Time: 11 ms (13.68%), Space: 13.8 MB (10.55%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,11 +1,29 @@
 class Solution {
+private:
+    int first_ind(int low, int high, vector<int> &arr, int &target) {
+        int ind = -1;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(arr[mid] == target) { ind = mid; high = mid-1; }
+            else if(arr[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return ind;
+    }
+    int last_ind(int low, int high, vector<int> &arr, int &target) {
+        int ind = -1;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(arr[mid] ==  target) { ind = mid; low = mid+1; }
+            else if(arr[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return ind;
+    }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        int x = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        if(n == 0 || x == n || nums[x] != target) return {-1, -1};
-        int y = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
-        
+        int x = first_ind(0, n-1, nums, target), y = last_ind(0, n-1, nums, target);
         return {x, y};
     }
 };
