b3692874660c20acff58d9b4b2268b089a67651e
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-09-25 22:55:27 +0530

    Time: 4 ms (48.11%), Space: 7 MB (16.46%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int ans = 0;
+        for(char ch: s) ans ^= ch;
+        for(char ch: t) ans ^= ch;
+        return (char)ans;
+    }
+};
