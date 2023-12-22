6f98ed3de4b3391f909b874e7277928771b2cad4
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-12-23 01:22:56 +0530

    Time: 22 ms (65.09%), Space: 15.6 MB (95.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,65 +1,21 @@
-class DisjointSet {
-    vector<int> rank, parent, size;
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-    int findUPr(int node) {
-        if (node == parent[node])
-            return node;
-        else
-            return parent[node] = findUPr(parent[node]);
-    }
-    void unionByRank(int u, int v) {
-        int upar_u = findUPr(u);
-        int upar_v = findUPr(v);
-        if (upar_u == upar_v)
-            return;
-        if (rank[upar_u] < rank[upar_v])
-            parent[upar_u] = upar_v;
-        else if (rank[upar_u] > rank[upar_v])
-            parent[upar_v] = upar_u;
-        else {
-            parent[upar_v] = upar_u;
-            rank[upar_u]++;
-        }
-    }
-    void unionBySize(int u, int v) {
-        int upar_u = findUPr(u);
-        int upar_v = findUPr(v);
-        if (upar_u == upar_v)
-            return;
-        if (size[upar_u] < size[upar_v]) {
-            parent[upar_u] = upar_v;
-            size[upar_v] += size[upar_u];
-        }
-        else {
-            parent[upar_v] = upar_u;
-            size[upar_u] += size[upar_v];
-        }
-    }
-};
 class Solution {
+private:
+    void dfs(int ind, int &n, vector<vector<int>>& isConnected, vector<bool> &vis) {
+        vis[ind] = true;
+        for (int j = 0; j < n; j++) {
+            if (isConnected[ind][j] && !vis[j]) dfs(j, n, isConnected, vis);
+        }
+    }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int V = isConnected.size(), province = 0;
-        DisjointSet ds(V);
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                if (isConnected[i][j] == 1) {
-                    ds.unionByRank(i, j);
-                }
+        int n = isConnected.size(), ans = 0;
+        vector<bool> vis(n, false);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                ans++;
+                dfs(i, n, isConnected, vis);
             }
         }
-        for (int i = 0; i < V; i++) {
-            if (ds.findUPr(i) == i) province++;
-        }
-        return province;
+        return ans;
     }
 };
