1cc96c515ff9185598bbb510cba68bb2382fa57a
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2023-12-23 01:36:30 +0530

    Time: 3 ms (96.39%), Space: 14.3 MB (82.80%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,20 +1,19 @@
 class Solution {
-public:
-    int rowArr[4] = {0, 0, -1, 1};
-    int colArr[4] = {-1, 1, 0, 0};
-    void dfs(int sr, int sc, int &color, int &prevColor, int &n, int &m, vector<vector<int>>& ans)
-    {
-        ans[sr][sc] = color;
-        for(int k = 0; k <= 3; k++)
-        {
-            int newRow = sr + rowArr[k], newCol = sc + colArr[k];
-            if(newRow >=  0 && newRow < n && newCol >=  0 && newCol < m && ans[newRow][newCol] == prevColor) dfs(newRow, newCol, color, prevColor, n, m, ans);
+private:
+    int delRow[4] = { -1, 0, 1, 0};
+    int delCol[4] = {0, 1, 0, -1};
+    void dfs(int row, int col, vector<vector<int>>& image, int &n, int &m, int &org_color, int &color) {
+        image[row][col] = color;
+        for (int k = 0; k < 4; k++) {
+            int nrow = row + delRow[k], ncol = col + delCol[k];
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == org_color) dfs(nrow, ncol, image, n, m, org_color, color);
         }
     }
+public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>> ans = image;
-        int prevColor = ans[sr][sc], n = image.size(), m = image[0].size();
-        if(prevColor != color) dfs(sr, sc, color, prevColor, n, m, ans);
-        return ans;
+        int n = image.size(), m = image[0].size(), org_color = image[sr][sc];
+        if (org_color == color) return image;
+        dfs(sr, sc, image, n, m, org_color, color);
+        return image;
     }
 };
