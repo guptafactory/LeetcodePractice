205b96f0bb74aa8f9a621f9ac41f34ca6445a2b8
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2024-01-10 03:59:26 +0530

    Time: 119 ms (93.17%), Space: 60.2 MB (97.19%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -8,15 +8,19 @@ private:
 public:
     int maximumSum(vector<int>& nums) {
         int sum = 0, ans = -1;
-        vector<vector<int>> arr(82);
+        vector<pair<int, int>> arr(82, pair<int, int> { -1, -1});
         for (int &x : nums) {
             sum = sum_digits(x);
-            arr[sum].push_back(x);
+            if (arr[sum].second == -1) arr[sum].second = x;
+            else if (arr[sum].second <= x) {
+                arr[sum].first = arr[sum].second;
+                arr[sum].second = x;
+            }
+            else arr[sum].first = max(arr[sum].first, x);
         }
         for (int i = 0; i < 82; i++) {
-            if (arr[i].size() <= 1) continue;
-            sort(arr[i].begin(), arr[i].end(), greater<int>());
-            ans = max(ans, arr[i][0] + arr[i][1]);
+            if (arr[i].first == -1) continue;
+            ans = max(ans, arr[i].first + arr[i].second);
         }
         return ans;
     }
