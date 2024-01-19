324383048170b8e168547a17e4a469fed8681580
Himanshu Gupta <97209900+guptafactory@users.noreply.github.com>
2024-01-20 03:51:22 +0530

    Time: 28 ms (8.98%), Space: 12.6 MB (5.08%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,62 +1,26 @@
 class Solution {
 private:
-    // top-down
-    int func1(int i, int j, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
-        if(i == 0) return matrix[0][j];
-        if(dp[i][j] != INT_MAX) return dp[i][j];
-        
-        int down = matrix[i][j] + func1(i-1, j, n, matrix, dp);
-        int down_left = j != 0 ? matrix[i][j] + func1(i-1, j-1, n, matrix, dp) : INT_MAX;
-        int down_right = j != n-1 ? matrix[i][j] + func1(i-1, j+1, n, matrix, dp) : INT_MAX;
-        
-        return dp[i][j] = min(min(dp[i][j], down), min(down_left, down_right));
-    }
-    // tabulation(bottom-up)
-    int func2(vector<vector<int>>& matrix, vector<vector<int>>& dp) {
-        int n = matrix.size();
-        for(int j = 0; j < n; j++) dp[0][j] = matrix[0][j]; // initialising 1st row
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                int down = matrix[i][j] + dp[i-1][j];
-                int down_left = j != 0 ? matrix[i][j] + dp[i-1][j-1] : INT_MAX;
-                int down_right = j != n-1 ? matrix[i][j] + dp[i-1][j+1] : INT_MAX;
-
-                dp[i][j] = min(min(dp[i][j], down), min(down_left, down_right));
-            }
-        }
-        int ans = INT_MAX;
-        for(int j = 0; j < n; j++) ans = min(ans, dp[n-1][j]);
-        return ans;
-    }
-    // optimisation
-    int func3(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        vector<int> prev(n), present(n);
-        for(int j = 0; j < n; j++) prev[j] = matrix[0][j]; // initialising 1st row
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                int down = matrix[i][j] + prev[j];
-                int down_left = j != 0 ? matrix[i][j] + prev[j-1] : INT_MAX;
-                int down_right = j != n-1 ? matrix[i][j] + prev[j+1] : INT_MAX;
-                
-                present[j] = min(down, min(down_left, down_right));
-            }
-            prev = present;
-        }
-        int ans = INT_MAX;
-        for(int x: prev) ans = min(x, ans);
-        return ans;
-    }
+    int delCol[3] = { -1, 0, 1};
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
-        int ans = INT_MAX;
-        for(int i = n-1, j = 0; j < n; j++) {
-            ans = min(ans, func1(i, j, n, matrix, dp));
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col < n; col++) {
+                int val = INT_MAX;
+                for (int k = 0; k < 3; k++) {
+                    int nrow = row + 1, ncol = col + delCol[k];
+                    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < n) val = min(val, matrix[nrow][ncol]);
+                }
+                matrix[row][col] += val;
+            }
         }
-        // return ans;
-        // return func2(matrix, dp);
-        return func3(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) cout << matrix[i][j] << ' ';
+            cout << endl;
+        }
+        int ans = INT_MAX;
+        for(int j = 0; j < n; j++) ans = min(ans, matrix[0][j]);
+        return ans;
+        // return *min(matrix[0].begin(), matrix[0].end());
     }
 };
